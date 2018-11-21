feaff7933b9ad1261b90730b7fc6f08c0eb97c71
Rohan <c1ph3r9@gmail.com>
2018-11-21 17:33:19 -0600

    adding remove and filter functions

--- TaskList-Project/app.js
+++ TaskList-Project/app.js
@@ -18,6 +18,15 @@ function loadEventListeners(){
   //add task event
 
   form.addEventListener('submit',addTask)
+  //remove task event
+
+  taskList.addEventListener('click', removeTask)
+
+  //clear task event
+
+  clearBtn.addEventListener('click',clearTasks)
+
+  filter.addEventListener('keyup', filterTasks)
 
 }
 
@@ -28,7 +37,69 @@ function addTask(e){
     alert('Add a task')
   }
 
+//create li item
 
+const li= document.createElement('li')
+//add class
+li.className='collection-item'
 
+//create text node and append to li
+
+li.appendChild(document.createTextNode(taskInput.value))
+//create a new link elem for delete
+
+const link=document.createElement('a')
+
+//add class
+link.className='delete-item secondary-content'
+//add icon
+link.innerHTML='<i class="fa fa-remove"></i>'
+//append the link to li
+
+li.appendChild(link)
+
+//append li to ul
+taskList.appendChild(li)
+
+//clear input
+taskList.value=""
   e.preventDefault();
+}
+
+function removeTask(e){
+
+  if(e.target.parentElement.classList.contains('delete-item')){
+    if(confirm('Are you sure')){
+      e.target.parentElement.parentElement.remove()
+
+    }
+    
+
+  }
+
+}
+//clear task
+function clearTasks(e){
+  taskInput.list.innerHTML=""
+
+  while(taskList.firstChild){
+    taskList.removeChild(taskList.firstChild)
+  }
+
+console.log(e)
+
+}
+
+function filterTasks(e){
+
+  const text=e.target.value.toLowerCase()
+  document.querySelectorAll('.collection-item').forEach((task)=>{
+    const item=task.firstChild.textContent
+    if(item.toLowerCase().indexOf(text) !==-1){
+      task.style.display='block'
+    }else{
+      task.style.display='none'
+    }
+  })
+
 }
